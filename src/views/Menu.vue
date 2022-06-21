dd2f3776ff5a8b71fb6ce43c97cd2d08763a4d07
satyamraj <44699922+Satyam-Raj@users.noreply.github.com>
2022-06-22 00:17:37 +0530

    menu update

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -3,7 +3,7 @@
     <v-card-actions class="justify-center">
       <v-chip color="grey lighten-3">
         <v-avatar> <v-icon>mdi-wallet</v-icon> &nbsp; &nbsp; </v-avatar>
-        <span> Ballance</span>&nbsp; &nbsp; <span> = Rs 110</span>&nbsp; &nbsp;
+        <span> Balance</span>&nbsp; &nbsp; <span> = Rs 110</span>&nbsp; &nbsp;
         <v-btn small rounded dark color="purple">Add</v-btn>
       </v-chip>
     </v-card-actions>
@@ -53,8 +53,8 @@
         </div>
       </v-expand-transition>
     </v-card>
-    <p>{{ radios || "null" }}</p>
 
+    
     <v-radio-group v-model="radios" mandatory>
       <v-row align="center" justify="center">
         <v-col cols="4" sm="3" md="2">
@@ -65,6 +65,50 @@
         </v-col>
       </v-row>
     </v-radio-group>
+
+    <v-card class="mx-auto" max-width="550">
+        <v-container>
+          <v-chip color="grey lighten-3">Starter</v-chip>
+          <v-container>
+            <v-row>
+              <v-col cols="4" sm="5" md="5">
+                <span>
+                  Manchurian
+                </span>
+              </v-col>
+              <v-col  cols="3" sm="3" md="3">
+                <span>
+                  100/-
+                </span>
+              </v-col>
+              <v-col align="center" cols="5" sm="4" md="4">
+                <span>
+                  <v-chip dark link color="black"><v-icon small dark>mdi-minus</v-icon></v-chip><v-chip>1</v-chip><v-chip dark link color="black"><v-icon small dark>mdi-plus</v-icon></v-chip>
+                </span>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-container>
+            <v-row>
+              <v-col cols="4" sm="5" md="5">
+                <span>
+                  Chai
+                </span>
+              </v-col>
+              <v-col  cols="3" sm="3" md="3">
+                <span>
+                  30/-
+                </span>
+              </v-col>
+              <v-col align="center" cols="5" sm="4" md="4">
+                <span>
+                  <v-chip dark link color="black"><v-icon small dark>mdi-minus</v-icon></v-chip><v-chip>2</v-chip><v-chip dark link color="black"><v-icon small dark>mdi-plus</v-icon></v-chip>
+                </span>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-container>
+    </v-card>
   </v-main>
 </template>
 
