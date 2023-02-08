6d453a12a503fd1be8b58fe0e7e019fb83a1a2e5
satyamraj <44699922+Satyam-Raj@users.noreply.github.com>
2023-02-09 00:10:43 +0530

    fixes

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import axios from "axios";
 Vue.prototype.$eventBus = new Vue();
 
 Vue.config.productionTip = false;
-axios.defaults.baseURL = "http://44.198.193.74:8000";
+axios.defaults.baseURL = "http://54.174.231.79:8000";
 
 
 new Vue({
