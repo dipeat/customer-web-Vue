3779c445461f18f4e1ed1270c153d8e662d8cb89
satyamraj <44699922+Satyam-Raj@users.noreply.github.com>
2023-02-12 18:02:20 +0530

    deploy conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
-const path = require("path");
-const webpack = require("webpack");
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 8080,
-    public: "dipeat.com",
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  // ... other configuration options ...
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    public: 'dipeat.com',
+    host: '0.0.0.0',
+    port: 8080
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
